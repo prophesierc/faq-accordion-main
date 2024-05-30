03de929ab8c2f09995feca4b88ca11c6dcda7c49
ProphesierC <bwknowles98@gmail.com>
2024-05-30 01:26:18 -0400

    main js finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,40 @@
 class Expand 
 {
+    
     constructor() 
     {
-        this.pNodeList = document.querySelectorAll('p');
-        this.imageNodeList = document.querySelectorAll('.content-section img');
-        this.imageNodeList.forEach((image, index) => 
-        {
-            image.addEventListener('click', () =>
-            {
-                pNodeList[this.index].style.display = 'flex';
-            });
-        })
+        this.pNodeList = document.querySelectorAll('.content-card > p');
+        this.imageNodeList = document.querySelectorAll('.content-section > img');
+        this.pNodeList.forEach(paragraphIndex => {
+            paragraphIndex.style.display = 'none';
+        });
+        this.imageNodeList.forEach(imageIndex => {
+            imageIndex.src = '/assets/images/icon-plus.svg';
+        });
     }
 
+    expand() 
+    {        
+        this.imageNodeList.forEach((image, index) => 
+        {            
+            image.addEventListener('click', () => 
+            {    
+                const paragraphIndex = this.pNodeList[index];
+                const imageIndex = this.imageNodeList[index];    
+                if (paragraphIndex.style.display === 'none') 
+                {
+                    imageIndex.src = '/assets/images/icon-minus.svg';
+                    paragraphIndex.style.display = 'flex';
+                } 
+                else 
+                {
+                    imageIndex.src = '/assets/images/icon-plus.svg';
+                    paragraphIndex.style.display = 'none';
+                }
+            });
+        });
+    }
 }
 
-const test = new Expand();
+const expandElement = new Expand();
+expandElement.expand();
