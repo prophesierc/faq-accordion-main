ce9d23e59adc4a6a2e6de273ce20faca51bf8e38
ProphesierC <bwknowles98@gmail.com>
2024-05-31 23:03:25 -0400

    Added onload event for initial display

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,35 @@ class Expand
     {
         this.pNodeList = document.querySelectorAll('.content-card > p');
         this.imageNodeList = document.querySelectorAll('.content-section > img');
+        this.firstImage = document.querySelector('.first-image');
         this.headerNodeList = document.querySelectorAll('.content-section > .header');
 
-        this.pNodeList.forEach(paragraphIndex => {
+        this.pNodeList.forEach(paragraphIndex => 
+        {
             paragraphIndex.style.display = 'none';
         });
-        this.imageNodeList.forEach(imageIndex => {
+        this.imageNodeList.forEach(imageIndex => 
+        {
             imageIndex.src = './assets/images/icon-plus.svg';
         });
-        this.headerNodeList.forEach(imageIndex => {
+        this.headerNodeList.forEach(imageIndex => 
+        {
             imageIndex.src = './assets/images/icon-plus.svg';
         });
+    }
+
+    initFirstHeader()
+    {
+        addEventListener('load', () => 
+        {
+            document.querySelector('.first-image').click()
+        })
     }
 
     nodeSelector()
     {
         this.imageNodeList.forEach((image, index) =>
-        {          
+        {      
             image.addEventListener('click', () => 
             {
                 this.expand(index)
@@ -50,9 +62,9 @@ class Expand
             imageIndex.src = './assets/images/icon-plus.svg';
             paragraphIndex.style.display = 'none';
         }
-            
     }
 }
 
 const expandElement = new Expand();
 expandElement.nodeSelector();
+expandElement.initFirstHeader();
